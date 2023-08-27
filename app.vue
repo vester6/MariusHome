6750a662199aa260ab3d099d4ffa4d5bf9778def
Mads Vester <vester@gmail.com>
2023-08-27 17:23:25 +0200

    first commit

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <h1>Hello world - this is marius!</h1>
-    <h1>Yeaaaah</h1>
+    <h1>This is marius homepage</h1>
+    <p>Here you can play fun games -Coming soon!</p>
   </div>
 </template>
-
-
