153d95afadf64152ab324fd5107ca79b2c57c8f1
Mads Vester <vester@gmail.com>
2023-08-27 19:08:05 +0200

    first commit3

--- app.vue
+++ app.vue
@@ -8,20 +8,37 @@
 
 <style>
 body {
-  background-color: rgb(
-    0,
-    162,
-    255
-  ); /* Ændr denne farve efter dine præferencer */
-  padding: 20px; /* Tilføj en smule polstring for at give plads omkring indholdet */
+  background-color: var(--bg-color, rgb(0, 162, 255));
+  padding: 20px;
+  transition: background-color 0.3s ease; /* Add transition for smooth color change */
 }
 
 .page-container {
-  background-color: rgb(
-    255,
-    255,
-    255
-  ); /* Ændr denne farve efter dine præferencer */
-  padding: 20px; /* Tilføj en smule polstring for at give plads omkring indholdet */
+  background-color: rgb(255, 255, 255);
+  padding: 20px;
 }
 </style>
+
+<script>
+export default {
+  methods: {
+    changeBackgroundColor() {
+      // Generate a random color
+      const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)})`;
+
+      // Apply the random color to the --bg-color custom property
+      document.documentElement.style.setProperty("--bg-color", randomColor);
+    },
+  },
+  mounted() {
+    // Add a click event listener to the body element
+    document.body.addEventListener("click", this.changeBackgroundColor);
+  },
+  beforeDestroy() {
+    // Remove the click event listener when the component is destroyed
+    document.body.removeEventListener("click", this.changeBackgroundColor);
+  },
+};
+</script>
